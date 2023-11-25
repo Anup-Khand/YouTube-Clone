0a0d7d5b84eb69c4050edd959a401db78905e8af
Anup-Khand <bk.basantkhand@gmail.com>
2023-11-25 09:57:08 +0545

    Adding Genre

--- src/components/Genre/Genre.jsx
+++ src/components/Genre/Genre.jsx
@@ -1,6 +1,44 @@
-import './Genre.css'
+import { useState } from "react";
+import "./Genre.css";
+const keywords = [
+  "All",
+  "React js",
+  "Redux",
+  "Music",
+  "Football",
+  "Cricket",
+  "Coding",
+  "use of API",
+  "Hindi Songs",
+  "Songs",
+  "Anime",
+  "Python",
+  "Angular js",
+  "LifeHacks",
+  "Science",
+  "Geography",
+  "History"
+];
 const Genre = () => {
-  return <div className="GenreContainer">Genre</div>;
+  const [activeElement, setactiveElement] = useState('All');
+  const handleclick = value => {
+    setactiveElement(value);
+  }
+  return (
+    <div className="GenreContainer">
+      <div className="Genre-detail">
+        {keywords.map((value, i) => (
+          <span
+            className={activeElement === value ? "active" : ""}
+            onClick={() => handleclick(value)}
+            key={i}
+          >
+            {value}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Genre;
